3ae5904e3bb688a27ce08baa9a7388e2d54658d3
Pragathi Shetty <pragathishetty504@gmail.com>
2024-07-21 18:22:03 +0530

    added file

---
+++ src/ex07062024/Lab004.java
@@ -0,0 +1,12 @@
+package ex07062024;
+
+public class Lab004 {
+    public static void main(String[] abs) {
+        //instead of args anything can be used it will work
+        //without main we can't run pgm
+        //if there are 2 main 1st one will be recognized
+        //can put[] near args
+        //instead of string int can't be added
+
+    }
+}
